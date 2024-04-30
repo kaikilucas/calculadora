826949775bbf20170ebd97b6e84fe1662aeac91a
kaikilucas <kaiki.lucasf@gmail.com>
2024-04-30 01:28:33 -0300

    mudei a logica

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,12 @@
 function calculadora(nome) {
   let valor = nome;
-
-  let resultado = parseInt(document.getElementById("menu"));
+  let resultado = document.getElementById("menu");
 
   if (resultado.value == "0") {
     resultado.value = valor;
   } else {
     resultado.value += valor;
   }
-
-  let final = resultado.value;
-  console.log(final);
-  if (valor == 999) {
-  }
-
-  if (valor == "AC") {
-    let AC = null;
-    resultado.value = 0;
-  }
+  console.log(resultado.value);
+  return parseFloat(resultado.value);
 }
